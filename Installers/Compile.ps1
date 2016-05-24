d0e71219ff78535913f99eb924d3bf3bf27cd31e
ESIT Build Account <bgcoebld@microsoft.com>
2016-05-24 03:27:03 -0700

    adding Installers
    adding installers folders

---
+++ Installers/Compile.ps1
@@ -0,0 +1,42 @@
+﻿<#                                   
+.SYNOPSIS
+Generic script to run PublishDrop
+.DESCRIPTION
+Generic script to run PublishDrop
+.EXAMPLE
+.\PublishDrop.ps1 -SourcesDirectory "D:\TB\Project\Branch\Sources" -DestinationPath "\\BuildMachineName\Release\Project\Branch"
+
+.EXAMPLE
+
+Version History  
+v1.0   - ESIT Build Team - Initial Release
+#>
+
+param(
+         [String]$SourcesDirectory=$(throw "pass: Sources directory.  Root location of bits to drop")
+         ,[String]$DestinationPath=$(throw "Pass : Destination (UNC) path")
+                  
+		 
+     )
+
+
+    
+try {
+#$SourcesDirectory="D:\ESIT_BuildManagement"
+& "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" "$SourcesDirectory\PilotProjects\GenericSolution\GenericTestCases.sln" /t:rebuild /p:Configuration=Release
+$BuildError1 = $LASTEXITCODE
+
+#Robocopy.exe "$SourcesDirectory\PilotProjects\GenericSolution\GenericTestCases\bin\Release" "$DestinationPath"
+
+exit 0;
+}
+
+catch{
+[System.Exception]
+ Write-Output $_.Exception.Message
+}
+
+finally{
+if ($BuildError1) { throw "msbuild failed to build GenericTestCases sln" }
+write-OutPut "pre compile executed successfully!"
+}
---
+++ Installers/precompile.ps1
@@ -0,0 +1,35 @@
+﻿<#                                   
+.SYNOPSIS
+Generic script to run PublishDrop
+.DESCRIPTION
+Generic script to run PublishDrop
+.EXAMPLE
+.\PublishDrop.ps1 -SourcesDirectory "D:\TB\Project\Branch\Sources" -DestinationPath "\\BuildMachineName\Release\Project\Branch"
+
+.EXAMPLE
+
+Version History  
+v1.0   - ESIT Build Team - Initial Release
+#>
+
+$SourcesDirectory=$Env:Build_SourcesDirectory
+#$BinariesDirectory="$Env:BUILD_ARTIFACTSTAGINGDIRECTORY"
+
+try {
+#$SourcesDirectory="D:\ESIT_BuildManagement"
+& "C:\Program Files (x86)\MSBuild\12.0\Bin\MSBuild.exe" "$SourcesDirectory\PilotProjects\GenericSolution\GenericTestCases.sln" /t:rebuild /p:Configuration=Release
+#$BuildError1 = $LASTEXITCODE
+
+#Robocopy.exe "$SourcesDirectory\PilotProjects\GenericSolution\GenericTestCases\bin\Release" "$DestinationPath\PilotProjects\GenericSolution\Sample" "*.*" -copyToBinariesFolder "Y"
+exit 0;
+}
+
+catch{
+[System.Exception]
+ Write-Output $_.Exception.Message
+}
+
+finally{
+ #if ($BuildError1) { throw "msbuild failed to build GenericTestCases sln" }
+write-OutPut "pre compile executed successfully!"
+}
